9b90e556ef7429b4296daef39eebaa59a0d78a7e
Manel Montilla <manel.montilla@gmail.com>
2023-05-13 20:10:04 +0200

    Broadcast challenge

---
+++ broadcast/broadcast.go
@@ -0,0 +1,256 @@
+package broadcast
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+// MessageWithBody defines the set of types that represent a message that can
+// be created from a body.
+type MessageWithBody[T any] interface {
+	*T
+	FromBody(map[string]any) error
+}
+
+// Message defines the set of types that a BatchBroadcaster can sent in
+// bacthes.
+type Message[T any] interface {
+	MessageWithBody[T]
+	Body() map[string]any
+	ID() string
+	Dest() string
+}
+
+// BatchBroadcaster sends messages to a specified destination node. It
+// accumulates the messages during a specified amount of time before it sends
+// them in a message of type broadcast_batch, retrying the operation if the
+// destination doesn't ack the message before a specified amount of time.
+type BatchBroadcaster[T any, P Message[T]] struct {
+	sync.RWMutex
+	Node       *maelstrom.Node
+	ACKTimeout time.Duration
+	BatchTime  time.Duration
+	BufferLen  int
+	processor  func(msg P) error
+	messages   chan P
+	done       chan struct{}
+}
+
+// NewBatchBroadcaster creates a new broadcaster that send messages in batches.
+// The broadcaster accumulates the messages for each different destination
+// during the specified batch time before sending them. The BatchBroadcaster
+// also will handle the batched broadcast messages by calling once the function
+// processor per each message in a batch.
+func NewBatchBroadcaster[T any, P Message[T]](ctx context.Context, node *maelstrom.Node, processor func(msg P) error) *BatchBroadcaster[T, P] {
+	b := &BatchBroadcaster[T, P]{
+		Node:       node,
+		processor:  processor,
+		BufferLen:  50,
+		ACKTimeout: 5 * time.Second,
+		BatchTime:  150 * time.Millisecond,
+	}
+	b.messages = make(chan P, b.BufferLen)
+	b.done = make(chan struct{}, 1)
+	// Register the handler for the batch broadcast messages.
+	node.Handle("batch_broadcast", b.handleBatchBroadcast)
+	go b.aggregateAndSend(ctx, b.BatchTime, b.done)
+	return b
+}
+
+func (b *BatchBroadcaster[T, P]) handleBatchBroadcast(msg maelstrom.Message) error {
+	log.Printf("BatchBroadcaster - handling batch message %v", msg)
+	var bMsg BatchBroadcastMessage[T, P]
+	if err := json.Unmarshal(msg.Body, &bMsg); err != nil {
+		return err
+	}
+	for _, msg := range bMsg.msgs {
+		if err := b.processor(msg); err != nil {
+			return err
+		}
+	}
+	return b.Node.Reply(msg, map[string]any{"type": "batch_broadcast_ok"})
+}
+
+// Send sends a Broadcast message with retries. This function is async unless
+// the buffer for sending the messages is full.
+func (b *BatchBroadcaster[T, P]) Send(msg P, dest string) {
+	// Detect if the channel is full.
+	select {
+	case b.messages <- msg:
+		return
+	default:
+		log.Printf("BatchBroadcaster - Messages buffer is full")
+	}
+	// Try to write again to backpreassure.
+	b.messages <- msg
+	log.Printf("Message buffered")
+}
+
+func (b *BatchBroadcaster[T, P]) aggregateAndSend(ctx context.Context, batchTime time.Duration, done chan<- struct{}) {
+	defer func() { done <- struct{}{} }()
+	msgByDest := map[string]map[string]P{}
+	var wg sync.WaitGroup
+	defer wg.Wait()
+	ticker := time.NewTicker(batchTime)
+	defer ticker.Stop()
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			log.Print("BatchBroadcaster - Stopping sending messages")
+			break loop
+		case msg := <-b.messages:
+			log.Printf("BatchBroadcaster - Sending message %+v, Dest: %v", msg, msg.Dest())
+			msgs, ok := msgByDest[msg.Dest()]
+			if !ok {
+				msgs = map[string]P{}
+			}
+			msgs[msg.ID()] = msg
+			msgByDest[msg.Dest()] = msgs
+		case <-ticker.C:
+			log.Print("BatchBroadcaster - Sending batch of messages")
+			for dst, messages := range msgByDest {
+				var batchOfMessages []P
+				for _, msg := range messages {
+					batchOfMessages = append(batchOfMessages, msg)
+				}
+				log.Printf("BatchBroadcaster - Batch message destination: %+v", dst)
+				msg := newBatchBroadcastMessage(dst, batchOfMessages)
+				wg.Add(1)
+				log.Printf("BatchBroadcaster - Sending batch message %+v", msg)
+				b.sendWithRetries(dst, msg.Body(), &wg, b.Node)
+			}
+			msgByDest = map[string]map[string]P{}
+		}
+	}
+}
+
+func (b *BatchBroadcaster[T, P]) sendWithRetries(dest string, body map[string]any, wg *sync.WaitGroup, node *maelstrom.Node) {
+	// TODO: implement a gracefully termination of the send and retry process
+	// by listening the Done channel of context received as a parameter.
+	go func() {
+		defer func() { wg.Done() }()
+		retry := 0
+	Loop:
+		for {
+			//log.Printf("Sending broadcast message: %+v, to: %+v, retry #%d\n", msg.Body(), msg.Dest(), retry)
+			ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(b.ACKTimeout))
+			defer cancel()
+			log.Printf("BatchBroadcaster - Sending message body %+v to: %s, retry #%d", body, dest, retry)
+			_, err := node.SyncRPC(ctx, dest, body)
+			if errors.Is(err, context.DeadlineExceeded) {
+				retry++
+				log.Printf("Timeout broadcasting message: %+v, to: %+v, starting retry #%d\n", body, dest, retry)
+				continue Loop
+			}
+			if err != nil {
+				log.Printf("Unexpected error sending broadcast message: %v, message: %+v, to: %+v, retry #%d\n", err, body, dest, retry)
+			} else {
+				log.Printf("Finished broadcast operation, message: %+v, to: %+v, retry #%d\n", body, dest, retry)
+			}
+			break
+		}
+	}()
+}
+
+// BatchBroadcastMessage is used by the batch broadcaster to send a batch of
+// messsages.
+type BatchBroadcastMessage[T any, P Message[T]] struct {
+	id   string
+	dest string
+	msgs []P
+}
+
+func newBatchBroadcastMessage[T any, P Message[T]](dst string, msgs []P) BatchBroadcastMessage[T, P] {
+	sort.Slice(msgs, func(i, j int) bool {
+		return strings.Compare(msgs[i].ID(), msgs[j].ID()) < 0
+	})
+	var id string
+	for _, msg := range msgs {
+		id = id + msg.ID()
+	}
+	return BatchBroadcastMessage[T, P]{
+		id:   id,
+		dest: dst,
+		msgs: msgs,
+	}
+}
+
+func (b BatchBroadcastMessage[T, P]) ID() string {
+	return b.id
+}
+
+func (b BatchBroadcastMessage[T, P]) Dest() string {
+	return b.dest
+}
+
+func (b BatchBroadcastMessage[T, P]) Body() map[string]any {
+	var messages []map[string]any
+	for _, v := range b.msgs {
+		messages = append(messages, v.Body())
+	}
+	body := map[string]any{
+		"type":     "batch_broadcast",
+		"messages": messages,
+	}
+	return body
+}
+
+// UnmarshalJSON unmarshals a [BatchBroadcastMessage].
+func (b *BatchBroadcastMessage[T, P]) UnmarshalJSON(data []byte) error {
+	var body map[string]any
+	err := json.Unmarshal(data, &body)
+	if err != nil {
+		return err
+	}
+	typ := body["type"]
+	if typ.(string) != "batch_broadcast" {
+		err := fmt.Errorf("invalid message type, expected type: batch_broadcast, got type: %v", typ)
+		return err
+	}
+
+	untypedBodies, ok := body["messages"]
+	if !ok {
+		err := errors.New("invalid message of type batch_broadcast, the messages field does not exist")
+		return err
+	}
+	bodies, ok := untypedBodies.([]any)
+	if !ok {
+		err := fmt.Errorf("invalid message of type batch_broadcast, unexpected messages type: %T", untypedBodies)
+		return err
+	}
+	var mapBodies []map[string]any
+	for _, b := range bodies {
+		mapBody, ok := b.(map[string]any)
+		if !ok {
+			err := fmt.Errorf("invalid message of type batch_broadcast, unexpected messages type: %T", b)
+			return err
+		}
+		mapBodies = append(mapBodies, mapBody)
+	}
+	var messages []P
+	for _, body := range mapBodies {
+		var msg T
+		log.Printf("Created empty message: %v, type: %T", msg, msg)
+		pMsg := P(&msg)
+		err := pMsg.FromBody(body)
+		if err != nil {
+			return err
+		}
+		messages = append(messages, pMsg)
+	}
+	broadcast := BatchBroadcastMessage[T, P]{
+		msgs: messages,
+	}
+	*b = broadcast
+	return nil
+}
---
+++ cmd/maelstrom-broadcast/main.go
@@ -0,0 +1,382 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"sync"
+
+	"github.com/bits-and-blooms/bitset"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+
+	"github.com/manelmontilla/maelstrom-challenges/broadcast"
+)
+
+func main() {
+	n := NewBroadcast()
+	if err := n.Run(); err != nil {
+		log.Fatalf("node stopped with err: %v", err)
+	}
+}
+
+type nodeIDs []nID
+
+func nodeIDsWithIDs(IDs []string) nodeIDs {
+	var neighbors []nID
+	for _, id := range IDs {
+		id := nID(id)
+		neighbors = append(neighbors, id)
+	}
+	return neighbors
+}
+
+func nodeIDsWithBitSet(bs *bitset.BitSet) nodeIDs {
+	var ids nodeIDs
+	for i, e := bs.NextSet(0); e; i, e = bs.NextSet(i + 1) {
+		var id nID
+		id = id.WithIndex(uint8(i))
+		ids = append(ids, id)
+	}
+	return ids
+}
+
+func (n nodeIDs) BitSet() (*bitset.BitSet, error) {
+	bs := bitset.New(uint(len(n)))
+	for _, neighbor := range n {
+		index, err := nID(neighbor).Index()
+		if err != nil {
+			return nil, err
+		}
+		bs.Set(uint(index))
+	}
+	return bs, nil
+}
+
+type nID string
+
+func (nID) WithIndex(i uint8) nID {
+	return nID(fmt.Sprintf("n%d", i))
+}
+
+func (n nID) Index() (uint8, error) {
+	// node ID examples: n1, n21
+	if len(n) < 2 {
+		return 0, fmt.Errorf("invalid node ID: %s", n)
+	}
+	strIndex := n[1:]
+	i, err := strconv.Atoi(string(strIndex))
+	if err != nil {
+		return 0, fmt.Errorf("invalid node ID %s: %w", n, err)
+	}
+	return uint8(i), nil
+}
+
+// Broadcast represents a broadcast server.
+type Broadcast struct {
+	*maelstrom.Node
+	neighbors   []nID
+	topology    Topology
+	messages    map[int]*bitset.BitSet
+	broadcaster *broadcast.BatchBroadcaster[BroadcastMessage, *BroadcastMessage]
+	sync.RWMutex
+}
+
+// NewBroadcast returns a new broadcast code ready to be run.
+func NewBroadcast() *Broadcast {
+	node := maelstrom.NewNode()
+	b := &Broadcast{
+		Node:     node,
+		messages: map[int]*bitset.BitSet{},
+		RWMutex:  sync.RWMutex{},
+		topology: DefaultTopology,
+	}
+	broadcaster := broadcast.NewBatchBroadcaster(context.Background(), node, b.processBroadcastMessage)
+	b.broadcaster = broadcaster
+	node.Handle("topology", b.HandleTopology)
+	node.Handle("read", b.HandleRead)
+	node.Handle("broadcast", b.HandleBroadcast)
+
+	return b
+
+}
+
+// HandleRead handles messages of type read.
+func (n *Broadcast) HandleRead(msg maelstrom.Message) error {
+	var body map[string]any
+	err := json.Unmarshal(msg.Body, &body)
+	if err != nil {
+		return err
+	}
+	n.RWMutex.RLock()
+	messages := make([]int, len(n.messages), len(n.messages))
+	log.Printf("Reading messages, messages in node: %+v, messages in response %+v",
+		n.messages, messages)
+	for k := range n.messages {
+		messages = append(messages, k)
+	}
+	n.RUnlock()
+	reply := map[string]any{
+		"type":     "read_ok",
+		"messages": messages,
+	}
+	return n.Reply(msg, reply)
+}
+
+// HandleBroadcast handles messages of type broadcast.
+func (n *Broadcast) HandleBroadcast(msg maelstrom.Message) error {
+	var bMsg BroadcastMessage
+	if err := json.Unmarshal(msg.Body, &bMsg); err != nil {
+		return err
+	}
+	err := n.processBroadcastMessage(&bMsg)
+	if err != nil {
+		return err
+	}
+	return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
+}
+
+// ProcessBroadcastMessage processes a broadcast message
+func (n *Broadcast) processBroadcastMessage(msg *BroadcastMessage) error {
+	var exist bool
+	n.Lock()
+	_, exist = n.messages[msg.Message]
+	if !exist {
+		length := len(n.NodeIDs())
+		bsInit := bitset.New(uint(length))
+		selfID := nID(n.ID())
+		selfIndex, err := selfID.Index()
+		if err != nil {
+			return err
+		}
+		bsInit = bsInit.Set(uint(selfIndex))
+		n.messages[msg.Message] = bsInit
+	}
+	msgSent := n.messages[msg.Message]
+	msgSent = msgSent.Union(&msg.Sent)
+
+	n.messages[msg.Message] = msgSent
+
+	alreadySent := msgSent.Clone()
+	neighbors := nodeIDs(n.neighbors)
+	n.Unlock()
+	if exist {
+		return nil
+	}
+	go func() {
+		alreadySentIDs := nodeIDsWithBitSet(alreadySent)
+		var sendTo []string
+		for _, n := range neighbors {
+			var found bool
+			for _, sID := range alreadySentIDs {
+				if sID == n {
+					found = true
+					break
+				}
+			}
+			if !found {
+				sendTo = append(sendTo, string(n))
+			}
+		}
+
+		sent, err := neighbors.BitSet()
+		if err != nil {
+			log.Printf("error in broadcast: %+v", err)
+		}
+		sent = sent.Union(alreadySent)
+		log.Printf("Broadcast Message: %d, already sent to: %+v, neighbors: %+v, sending to: %+v", msg.Message, alreadySentIDs, neighbors, sendTo)
+		for _, s := range sendTo {
+
+			nodeMsg := BroadcastMessage{
+				Destination: s,
+				Message:     msg.Message,
+				Sent:        *sent,
+			}
+			n.broadcaster.Send(&nodeMsg, s)
+		}
+	}()
+	return nil
+}
+
+// HandleTopology handles messages of type topology.
+func (n *Broadcast) HandleTopology(msg maelstrom.Message) error {
+	var topologyMsg TopologyMessage
+	if err := json.Unmarshal(msg.Body, &topologyMsg); err != nil {
+		return fmt.Errorf("error unmarshalling topology: %w", err)
+	}
+	neighbors, err := n.topology(n.Node, msg, topologyMsg.Topology)
+	if err != nil {
+		return err
+	}
+	n.Lock()
+	n.neighbors = nodeIDsWithIDs(neighbors)
+	n.Unlock()
+	n.Node.Reply(msg, map[string]any{"type": "topology_ok"})
+	return nil
+}
+
+// Topology defines a functions that returns the list of neighbors of a a node
+// given the current node and the topology info received in a Topology message.
+type Topology func(node *maelstrom.Node, msg maelstrom.Message, topology map[string][]string) ([]string, error)
+
+// DefaultTopology represents the topology used by the node when no other
+// topology has been specified.
+func DefaultTopology(node *maelstrom.Node, msg maelstrom.Message, topology map[string][]string) ([]string, error) {
+	neighbors, ok := topology[node.ID()]
+	if !ok {
+		err := fmt.Errorf("no information for the current node: %s, in the topology info", node.ID())
+		return nil, err
+	}
+	return neighbors, nil
+}
+
+// Run starts the inner maelstrom sever.
+func (n *Broadcast) Run() error {
+	return n.Node.Run()
+}
+
+// BroadcastMessage represents the message received in a broadcast operation.
+type BroadcastMessage struct {
+	MsgID       int    `json:"msg_id,omitempty"`
+	Destination string `json:"destination,omitempty"`
+	Message     int    `json:"message,omitempty"`
+	// Sent contatins the set of nodes that this message has been sent so far.
+	Sent bitset.BitSet `json:"sent,omitempty"`
+}
+
+// Body returns the body of the messsage as a map.
+func (b BroadcastMessage) Body() map[string]any {
+	buff, _ := b.Sent.MarshalBinary()
+	sent := base64.StdEncoding.EncodeToString(buff)
+	body := map[string]any{
+		"type":    "broadcast",
+		"message": b.Message,
+		"sent":    sent,
+	}
+	if b.MsgID != 0 {
+		body["msg_id"] = b.MsgID
+	}
+	return body
+}
+
+// ID returns and ID that uniquely represents the payload of the message.
+func (b BroadcastMessage) ID() string {
+	return strconv.FormatInt(int64(b.Message), 10)
+}
+
+// Dest returns the dest node of the message.
+func (b BroadcastMessage) Dest() string {
+	return b.Destination
+}
+
+// MarshalJSON marshals a [BroadcastMessage].
+func (b BroadcastMessage) MarshalJSON() ([]byte, error) {
+	return json.Marshal(b.Body())
+}
+
+// FromBody unmarshals a [BroadcastMessage] from a map.
+func (b *BroadcastMessage) FromBody(body map[string]any) error {
+	typ := body["type"]
+	if typ.(string) != "broadcast" {
+		err := fmt.Errorf("invalid message of type broadcast, expected message type broadcast, got type: %v", typ)
+		return err
+	}
+	rawMessage, ok := body["message"]
+	if !ok {
+		err := errors.New("invalid message of type broadcast, the message field does not exist")
+		return err
+	}
+	payload := int(rawMessage.(float64))
+	msgID := 0
+	if id, ok := body["msg_id"]; ok {
+		fMsgID, ok := id.(float64)
+		if !ok {
+			err := fmt.Errorf("invalid message of type broadcast, the message field msg_id is not a string, is a: %T", id)
+			return err
+		}
+		msgID = int(fMsgID)
+	}
+
+	var sentBitSet bitset.BitSet
+	sent, ok := body["sent"]
+	if ok {
+		sentStr, ok := sent.(string)
+		if !ok {
+			err := fmt.Errorf("invalid message of type broadcast, the message field sent is not a string, is a: %T", sent)
+			return err
+		}
+		sentBytes, err := base64.StdEncoding.DecodeString(sentStr)
+		if err != nil {
+			err := fmt.Errorf("invalid message of type broadcast, the message field sent: %s, can't be decoded: %+w, ", sentStr, err)
+			return err
+		}
+		err = sentBitSet.UnmarshalBinary(sentBytes)
+		if err != nil {
+			err := fmt.Errorf("invalid message of type broadcast, the message field sent: %s, can't be decoded: %+w, ", sentBytes, err)
+			return err
+		}
+	}
+
+	broadcast := BroadcastMessage{
+		Message: payload,
+		MsgID:   msgID,
+		Sent:    sentBitSet,
+	}
+	*b = broadcast
+	return nil
+}
+
+// UnmarshalJSON unmarshals a [BroadcastMessage].
+func (b *BroadcastMessage) UnmarshalJSON(data []byte) error {
+	var body map[string]any
+	err := json.Unmarshal(data, &body)
+	if err != nil {
+		return err
+	}
+	return b.FromBody(body)
+}
+
+// TopologyMessage represents a message received in a topology operation.
+type TopologyMessage struct {
+	Topology map[string][]string
+}
+
+// UnmarshalJSON unmarshals a [TopologyMessage].
+func (t *TopologyMessage) UnmarshalJSON(data []byte) error {
+	var msg map[string]interface{}
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return err
+	}
+	topology, ok := msg["topology"]
+	if !ok {
+		return errors.New("invalid topology message")
+	}
+	tmap, ok := topology.(map[string]interface{})
+	if !ok {
+		return errors.New("invalid topology payload")
+	}
+	if len(tmap) == 0 {
+		return nil
+	}
+	topo := make(map[string][]string, len(tmap))
+	for k, v := range tmap {
+		values, ok := v.([]interface{})
+		if !ok {
+			return fmt.Errorf("invalid topology values: %+v, values type: %T", v, v)
+		}
+		strVals := make([]string, 0, len(values))
+		for _, value := range values {
+			vstr, ok := value.(string)
+			if !ok {
+				return fmt.Errorf("invalid topology string value: %+v, values type: %T", value, value)
+			}
+			strVals = append(strVals, vstr)
+		}
+		topo[k] = strVals
+	}
+	t.Topology = topo
+	return nil
+}
